b81ad3fa67f5defe4ad1ce76bbdba49fceaa56c4
Pakorn Penboonmee <xenomous@Pinnidas-MacBook-Pro.local>
2021-03-28 02:30:31 +0700

    feat: update correct statment

--- app/Http/Services/CardService.php
+++ app/Http/Services/CardService.php
@@ -1,16 +1,14 @@
 <?php
 
-use App\Http\Repositories\CardRepository;
-
 namespace App\Http\Services;
+
+use App\Http\Repositories\CardRepository;
 
 class CardService
 {
     private $cardRepository;
 
-    public function __construct(
-        $cardRepository CardRepository
-    ) {
+    public function __construct(CardRepository $cardRepository) {
         $this->cardRepository = $cardRepository;
     }
 } 
